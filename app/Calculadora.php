dcb31c80a8cb00e655c37a30330d52278abf946b
Dsblima <dsbl@ecomp.poli.br>
2019-02-08 01:23:53 -0200

    Testando Atributos

---
+++ app/Calculadora.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+class Calculadora
+{
+    private $valorA;
+    private $valorB;
+    private $operador;
+    private $resultado;
+    
+}
+
+?>
---
+++ tests/CalculadoraTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Calculadora;
+
+class CalculadoraTest extends TestCase
+{
+    public function testAtributosCalculadora()
+    {
+        $this->assertClassHasAttribute('valorA',Calculadora::class);
+        $this->assertClassHasAttribute('valorB',Calculadora::class);
+        $this->assertClassHasAttribute('operador',Calculadora::class);
+        $this->assertClassHasAttribute('resultado',Calculadora::class);
+    }
+}
+
+?>
